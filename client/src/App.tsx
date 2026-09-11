ed031fb9adbb9bb6ba59fe5ac88b051c04726140
efstechnologyapp <efstechnology.app@gmail.com>
2026-09-11 10:59:54 -0300

    Replace Router component with Home component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,9 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import InstallApp from "./components/InstallApp";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
 
 export default function App() {
   return (
@@ -22,7 +11,7 @@ export default function App() {
       <ThemeProvider defaultTheme="light">
         <TooltipProvider>
           <Toaster position="bottom-right" richColors />
-          <Router />
+          <Home />
           <InstallApp />
         </TooltipProvider>
       </ThemeProvider>
