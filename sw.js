7c42c6fbf59deb637253183e0521eb775da2e884
efstechnologyapp <efstechnology.app@gmail.com>
2026-09-17 05:42:51 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nutritrack-v50';
+const CACHE_NAME = 'nutritrack-v51';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
